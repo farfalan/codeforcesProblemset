0fc7da5a3f215971b484112617fe72110ef1834b
farfalan <9ar9alan@gmail.com>
2026-01-25 12:13:42 +0100

    B.Dreamoon_and_WiFi.py

--- B.Dreamoon_and_WiFi.py
+++ B.Dreamoon_and_WiFi.py
@@ -1,16 +1,12 @@
-
+#https://codeforces.com/problemset/problem/476/B
 # sender  = input()
 # receiver = input()
+import math
+sender = "+--++"
+receiver = "--+??"
 
-# sender = "+--++"
-# receiver = "+-+-?"
-
-sender = "+++"
-receiver = "??-"
-
-
-
-print(receiver.count("?"))
+# sender = "+++"
+# receiver = "??-"
 
 def getfinalstep(steps):
     s = 0
@@ -25,13 +21,21 @@ max = getfinalstep(receiver) + receiver.count("?")
 min = getfinalstep(receiver) - receiver.count("?")
 
 if getfinalstep(sender) <= max and getfinalstep(sender) >= min:
-    print("im Bereich")
+
+    # formel beschreibt wieviele steps (+) (-) sind benötigt vom atuellen Stand des receivers (temp) um auf den finalen stand des Sender (s)
+    # zu kommen.
+    # n beschreibt Anzahl von "?" 
+    # s = k . (+1) + (n - k) (-1) + temp  => k = (s + n + temp)/2
+
+    s = getfinalstep(sender)
+    n = receiver.count("?")
+    temp = getfinalstep(receiver)
+
+    k = int((s + n - temp)/2)
+
+    #p1 = math.comb(n, k) * (0.5)**n
+    p = (math.factorial(n) / (math.factorial(k) * (math.factorial(n - k)))) * (1/2)**k * (1/2)**(n - k)
+
+    print(p)
 else:
-      print("nicht im Bereich")  
-
-print(getfinalstep(sender))
-print(getfinalstep(receiver))
-
-
-
-
+      print("nicht im Bereich")
